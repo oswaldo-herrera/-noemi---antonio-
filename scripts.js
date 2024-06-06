20a5b5cba3098572176780db83046341769b8aad
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2024-06-06 14:15:57 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const targetDate = new Date('August 3, 2024 00:00:00').getTime();
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const timeLeft = targetDate - now;
+
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById('days').innerText = days;
+        document.getElementById('hours').innerText = hours;
+        document.getElementById('minutes').innerText = minutes;
+        document.getElementById('seconds').innerText = seconds;
+    }
+
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const timelineEvents = document.querySelectorAll('.timeline-event');
+
+    window.addEventListener('scroll', function() {
+        let top = window.scrollY + (window.innerHeight / 2); // Posición del scroll más la mitad de la ventana
+
+        timelineEvents.forEach(function(event) {
+            if (top > event.offsetTop) {
+                event.classList.add('active');
+            } else {
+                event.classList.remove('active');
+            }
+        });
+    });
+});
